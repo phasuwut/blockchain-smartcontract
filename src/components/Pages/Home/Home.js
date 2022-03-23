9f88dc8acabea2fb05e5643b95fc53494e0098dd
phasuwut-ogilvy <86759240+phasuwut-ogilvy@users.noreply.github.com>
2022-03-23 22:22:54 +0700

    add lib  tailwindcss

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div>
+			Home
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		</div>
+	);
 };
 
 export default Home;
