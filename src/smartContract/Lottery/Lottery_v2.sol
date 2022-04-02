2031bd1763e2cfb834d24c0fbd97aedccdb3513c
phasuwut <44774302+phasuwut@users.noreply.github.com>
2022-04-02 22:04:07 +0700

    update Lottery_v2.sol

--- src/smartContract/Lottery/Lottery_v2.sol
+++ src/smartContract/Lottery/Lottery_v2.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
-import "@openzeppelin/contracts/utils/Strings.sol";
+
+
 /** 
  * @title Lottery
  * @dev Implements voting process along with vote delegation
@@ -27,14 +28,15 @@ contract Lottery {
 
     function buyersRegister(string memory firstName, string memory lastName,string memory email) public {
         // firstName, lastName, email ต้องไม่เป็นค่าว่าง
-        require(!checkStringEqualNull(buyerStruct[msg.sender].email), "firstName !== null"); 
-        require(!checkStringEqualNull(buyerStruct[msg.sender].lastName), "lastName !== null");  
-        require(!checkStringEqualNull(buyerStruct[msg.sender].email), "email !== null"); 
+    
+       
+        require(!checkStringEqualNull(email), "firstName !== null"); 
+        require(!checkStringEqualNull(lastName), "lastName !== null");  
+        require(!checkStringEqualNull(email), "email !== null"); 
 
-        Buyer storage buyer = buyerStruct[msg.sender];
-        buyer.firstName = firstName;
-        buyer.lastName = lastName;
-        buyer.email = email;
+        buyerStruct[msg.sender].firstName = firstName;
+        buyerStruct[msg.sender].lastName = lastName;
+        buyerStruct[msg.sender].email = email;
 
         buyer_result.push(msg.sender);
     }
@@ -51,7 +53,7 @@ contract Lottery {
         return buyerStruct[msg.sender];
     }
     modifier checkRegistor() {
-       checkStringEqualNull(buyerStruct[msg.sender].email);
+       require(!checkStringEqualNull(buyerStruct[msg.sender].email), "Please register");
         _;
     }
 
@@ -60,7 +62,7 @@ contract Lottery {
 
     //  Lottery
     struct Lottery_ { // ข้อมูล
-        uint lotteryNo; // หมายเลขหวย
+        string lotteryNo; // หมายเลขหวย
         string period; // ช่วงวันที่ 
         uint amount;
         address [] listAddress; // address ของคนซื้อ 
@@ -71,8 +73,8 @@ contract Lottery {
 
 
     // กำหนดค่าเริ่มต้น 
-    function LotteryRegister(uint lotteryNo, string memory period) private {
-        string memory _address = concatenate(Strings.toString(lotteryNo),period); /// PK
+    function LotteryRegister(string memory lotteryNo, string memory period) private {
+        string memory _address = concatenate(lotteryNo,period); /// PK
 
         lotteryStruct[_address].lotteryNo = lotteryNo; // หมายเลขของหวย ที่ต้องการซื้อ
         lotteryStruct[_address].period = period; // งวดวันที่ 
@@ -91,14 +93,15 @@ contract Lottery {
         return lottery_result.length;
     }
 
-    function buyingLottery(uint lotteryNo, string memory period) public checkRegistor(){
+    function buyingLottery(string memory lotteryNo, string memory period) public checkRegistor(){
         // พวกการเช็ค่าต่างๆ ยังไม่ได้ทำ
-        require(!checkStringEqualNull(Strings.toString(lotteryNo)), "lotteryNo !== null"); 
-        require(!checkStringEqualNull(period), "lotteryNo !== null"); 
+        require(!checkStringEqualNull(period), "period !== null"); 
+        require(!checkStringEqualNull(lotteryNo), "lotteryNo !== null"); 
 
-        require(checkNumberAndGenerateLottery(lotteryNo,period), "not buy2");
 
-        string memory _address = concatenate(Strings.toString(lotteryNo),period); /// PK
+        //require(!checkNumber(lotteryNo,period), "not buy2");
+
+        string memory _address = concatenate(lotteryNo,period); /// PK
        
         lotteryStruct[ _address].amount = lotteryStruct[ _address].amount-1;
         lotteryStruct[ _address].listAddress.push(msg.sender);  // address ของคนซื้อ 
@@ -106,15 +109,15 @@ contract Lottery {
     }
 
     //check ว่าเลขนี้มีอยู่หรือป่าว
-    function checkNumber(uint lotteryNo, string memory period) public view returns (bool){
-        if(lotteryNo>LotteryMax){
+    function checkNumber(string memory lotteryNo, string memory period) public view returns (bool){
+        if(st2num(lotteryNo)>LotteryMax){ 
             return false;
-        }else if(lotteryNo==0){
+        }else if(st2num(lotteryNo)==0){
             return false;
         }
 
-        string memory _address = concatenate(Strings.toString(lotteryNo),period);
-        if( lotteryStruct[_address].lotteryNo==lotteryNo){ // เจอว่ามีเลขนี้อยู่นะ
+        string memory _address = concatenate(lotteryNo,period);
+        if( checkStringEqual(lotteryStruct[_address].lotteryNo,lotteryNo)){ // เจอว่ามีเลขนี้อยู่นะ
             return true; 
         }else{
             return false; 
@@ -122,7 +125,7 @@ contract Lottery {
     }
 
     //check ว่าเลขนี้มีอยู่หรือป่าว ถ้าไม่มีให้สร้างขึ้นมาใหม่ แต่ต้ามสร้างเกินเลขที่กำหนดไว่า
-    function checkNumberAndGenerateLottery(uint lotteryNo, string memory period) private returns (bool){
+    function checkNumberAndGenerateLottery(string memory lotteryNo, string memory period) private returns (bool){
         if(checkNumber(lotteryNo,period)){
              LotteryRegister(lotteryNo,period);
              return true;
@@ -130,6 +133,8 @@ contract Lottery {
             return false;
         }
     }
+
+
 
 
 
@@ -153,7 +158,26 @@ contract Lottery {
         }
          return false;
     }
-
+    function checkStringEqual(string memory string1, string memory string2) public pure returns (bool){ 
+        if( keccak256(abi.encodePacked(string1)) == keccak256(abi.encodePacked(string2)) ){
+            return true;
+        }
+         return false;
+    }
+    function st2num(string memory numString) public pure returns(uint) {
+        uint  val=0;
+        bytes   memory stringBytes = bytes(numString);
+        for (uint  i =  0; i<stringBytes.length; i++) {
+            uint exp = stringBytes.length - i;
+            bytes1 ival = stringBytes[i];
+            uint8 uval = uint8(ival);
+           uint jval = uval - uint(0x30);
+   
+           val +=  (uint(jval) * (10**(exp-1))); 
+        }
+      return val;
+    }
 
 }
+
 
