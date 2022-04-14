3d669573dc63c18d2b3fa9777cfd3fffebf1e97b
phasuwut <44774302+phasuwut@users.noreply.github.com>
2022-04-14 21:37:53 +0700

    update

--- README.md
+++ README.md
@@ -13,3 +13,12 @@ npx hardhat node
 node scripts/sample-script.js
 npx hardhat help
 ```
+
+
+1 =>  npx hardhat compile
+2 => npx hardhat run scripts/deploy.js --network ropsten
+Contract deployed to address: 0xA6f34783d075989AF0EB526A911Bf357bD6E7FeA
+
+
+
+
---
+++ scripts/interact.js
@@ -0,0 +1,34 @@
+// interact.js
+
+const API_KEY = process.env.API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+const contract = require("../artifacts/contracts/Lottery.sol/Lottery.json");
+console.log(JSON.stringify(contract.abi));
+
+// interact.js
+
+// Provider
+const alchemyProvider = new ethers.providers.AlchemyProvider(
+  (network = "ropsten"),
+  API_KEY
+);
+
+// Signer
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+// Contract
+const lotteryContract = new ethers.Contract(
+  CONTRACT_ADDRESS,
+  contract.abi,
+  signer
+);
+
+
+
+async function main() {
+  const message = await lotteryContract. getMyBalance();
+  console.log("The message is: " + message);
+}
+main();
