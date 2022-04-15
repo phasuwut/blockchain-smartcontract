e95f2afd6c821294fc9e0b1e94a91542f28cd4fc
phasuwut <44774302+phasuwut@users.noreply.github.com>
2022-04-16 02:31:58 +0700

    add buyingLottery

--- src/components/Pages/BackOffice/components/Manager/Manager.js
+++ src/components/Pages/BackOffice/components/Manager/Manager.js
@@ -1,31 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 
-import { generateLottery } from "../../../../../util/lottery";
+import { generateLottery, getPeriodAll } from "../../../../../util/lottery";
+import { Button, Form } from "react-bootstrap";
 
 const Manager = ({ myAddress }) => {
 	const [period, setPeriod] = useState("");
+	const [periodAll, setPeriodAll] = useState([]);
+
 	const handleonSubmitGenerateLottery = (event) => {
 		event.preventDefault();
-        console.log(period)
+		console.log(period);
 		generateLottery(myAddress, period).then((res) => {
 			console.log(res);
 		});
 	};
+	useMemo(() => {
+		getPeriodAll().then((res) => {
+			console.log(res);
+			setPeriodAll(res);
+		});
+	}, []);
 	return (
 		<div>
 			<h1>BackOffice manager</h1>
-			<form onSubmit={handleonSubmitGenerateLottery}>
-				<p>generateLottery</p>
-				<input
-					placeholder="period"
-					type={"text"}
-					value={period}
-					onChange={(event) => {
-						setPeriod(event.target.value);
-					}}
-				/>
-				<button> generateLottery</button>
-			</form>
+			<hr />
+			<h4>PeriodAll</h4>
+			<ul>
+				{periodAll.map((item, i) => {
+					return <li key={i}>{item}</li>;
+				})}
+			</ul>
+			<hr />
+			<Form onSubmit={handleonSubmitGenerateLottery}>
+				<Form.Group className="mb-3">
+					<Form.Label>Period</Form.Label>
+					<Form.Control
+						placeholder="period"
+						type={"text"}
+						value={period}
+						onChange={(event) => {
+							setPeriod(event.target.value);
+						}}
+					/>
+				</Form.Group>
+
+				<Button variant="primary" type="submit">
+					Submit
+				</Button>
+			</Form>
+			<hr />
 		</div>
 	);
 };
