04d80706be40dbb6281cd2804561e53b49ed052f
phasuwut <44774302+phasuwut@users.noreply.github.com>
2022-04-24 12:56:05 +0700

    update Manager Accordion

--- src/components/Gobal/Flex/Flex.js
+++ src/components/Gobal/Flex/Flex.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
-const Flex = ({ children }) => {
-	return <CustomFlex>{children}</CustomFlex>;
+const Flex = ({ children , onClick}) => {
+	return <CustomFlex onClick={onClick}>{children}</CustomFlex>;
 };
 const CustomFlex = styled.div`
 	display: flex;
--- src/components/Pages/BackOffice/components/Manager/Manager.js
+++ src/components/Pages/BackOffice/components/Manager/Manager.js
@@ -10,6 +10,8 @@ const Manager = ({ myAddress }) => {
 	const [period, setPeriod] = useState("");
 	const [status, setStatus] = useState("");
 
+	const [showAccordion, setShowAccordion] = useState("0");
+
 	const handleonSubmitGenerateLottery = (event) => {
 		event.preventDefault();
 		generateLottery(myAddress, period).then((res) => {
@@ -23,25 +25,50 @@ const Manager = ({ myAddress }) => {
 			<h1>BackOffice manager</h1>
 			<hr />
 
-			<div>
+			<Wapper>
 				<Accordion defaultActiveKey="0">
 					<Accordion.Item eventKey="0">
 						<CustomAccordionHeader>
-							<Flex>
+							<Flex
+								onClick={() => {
+									console.log("0");
+									if(showAccordion === "0"){
+										setShowAccordion("");
+									}else{
+										setShowAccordion("0");
+									}
+								}}
+							>
 								<h1>Period ทั้งหมด</h1>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="50"
-									height="50"
-									fill="currentColor"
-									class="bi bi-arrow-bar-down"
-									viewBox="0 0 16 16"
-								>
-									<path
-										fill-rule="evenodd"
-										d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z"
-									/>
-								</svg>
+								{showAccordion === "0" ? (
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="50"
+										height="50"
+										fill="currentColor"
+										class="bi bi-arrow-bar-up"
+										viewBox="0 0 16 16"
+									>
+										<path
+											fill-rule="evenodd"
+											d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5zm-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5z"
+										/>
+									</svg>
+								) : (
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="50"
+										height="50"
+										fill="currentColor"
+										class="bi bi-arrow-bar-down"
+										viewBox="0 0 16 16"
+									>
+										<path
+											fill-rule="evenodd"
+											d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z"
+										/>
+									</svg>
+								)}
 							</Flex>
 						</CustomAccordionHeader>
 						<Accordion.Body>
@@ -50,21 +77,47 @@ const Manager = ({ myAddress }) => {
 					</Accordion.Item>
 					<Accordion.Item eventKey="1">
 						<CustomAccordionHeader>
-							<Flex>
+							<Flex
+								onClick={() => {
+									console.log("1");
+									if(showAccordion === "1"){
+										setShowAccordion("");
+									}else{
+										setShowAccordion("1");
+									}
+								
+								}}
+							>
 								<h1>Ganarate Lorrery</h1>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="50"
-									height="50"
-									fill="currentColor"
-									class="bi bi-arrow-bar-down"
-									viewBox="0 0 16 16"
-								>
-									<path
-										fill-rule="evenodd"
-										d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z"
-									/>
-								</svg>
+								{showAccordion === "1" ? (
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="50"
+										height="50"
+										fill="currentColor"
+										class="bi bi-arrow-bar-up"
+										viewBox="0 0 16 16"
+									>
+										<path
+											fill-rule="evenodd"
+											d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5zm-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5z"
+										/>
+									</svg>
+								) : (
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="50"
+										height="50"
+										fill="currentColor"
+										class="bi bi-arrow-bar-down"
+										viewBox="0 0 16 16"
+									>
+										<path
+											fill-rule="evenodd"
+											d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z"
+										/>
+									</svg>
+								)}
 							</Flex>
 						</CustomAccordionHeader>
 						<Accordion.Body>
@@ -94,8 +147,7 @@ const Manager = ({ myAddress }) => {
 						</Accordion.Body>
 					</Accordion.Item>
 				</Accordion>
-			</div>
-
+			</Wapper>
 			<hr />
 		</div>
 	);
@@ -105,4 +157,7 @@ const CustomAccordionHeader = styled(Accordion.Header)`
 		width: 100%;
 	}
 `;
+
+const Wapper = styled.div``;
+
 export default Manager;
