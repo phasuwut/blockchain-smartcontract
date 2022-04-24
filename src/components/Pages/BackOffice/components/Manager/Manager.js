963b8f81cc169e7522e6bc7506eadbc0e8f8ad00
phasuwut <44774302+phasuwut@users.noreply.github.com>
2022-04-24 15:23:00 +0700

    update BackOffice manager

--- src/components/Pages/BackOffice/components/Manager/Manager.js
+++ src/components/Pages/BackOffice/components/Manager/Manager.js
@@ -32,9 +32,9 @@ const Manager = ({ myAddress }) => {
 							<Flex
 								onClick={() => {
 									console.log("0");
-									if(showAccordion === "0"){
+									if (showAccordion === "0") {
 										setShowAccordion("");
-									}else{
+									} else {
 										setShowAccordion("0");
 									}
 								}}
@@ -46,12 +46,12 @@ const Manager = ({ myAddress }) => {
 										width="50"
 										height="50"
 										fill="currentColor"
-										class="bi bi-arrow-bar-up"
+										class="bi bi-arrow-down"
 										viewBox="0 0 16 16"
 									>
 										<path
 											fill-rule="evenodd"
-											d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5zm-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5z"
+											d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
 										/>
 									</svg>
 								) : (
@@ -60,12 +60,12 @@ const Manager = ({ myAddress }) => {
 										width="50"
 										height="50"
 										fill="currentColor"
-										class="bi bi-arrow-bar-down"
+										class="bi bi-arrow-up"
 										viewBox="0 0 16 16"
 									>
 										<path
 											fill-rule="evenodd"
-											d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z"
+											d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
 										/>
 									</svg>
 								)}
@@ -75,17 +75,17 @@ const Manager = ({ myAddress }) => {
 							<Award />
 						</Accordion.Body>
 					</Accordion.Item>
+					<hr />
 					<Accordion.Item eventKey="1">
 						<CustomAccordionHeader>
 							<Flex
 								onClick={() => {
 									console.log("1");
-									if(showAccordion === "1"){
+									if (showAccordion === "1") {
 										setShowAccordion("");
-									}else{
+									} else {
 										setShowAccordion("1");
 									}
-								
 								}}
 							>
 								<h1>Ganarate Lorrery</h1>
@@ -95,12 +95,12 @@ const Manager = ({ myAddress }) => {
 										width="50"
 										height="50"
 										fill="currentColor"
-										class="bi bi-arrow-bar-up"
+										class="bi bi-arrow-down"
 										viewBox="0 0 16 16"
 									>
 										<path
 											fill-rule="evenodd"
-											d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5zm-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5z"
+											d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
 										/>
 									</svg>
 								) : (
@@ -109,12 +109,12 @@ const Manager = ({ myAddress }) => {
 										width="50"
 										height="50"
 										fill="currentColor"
-										class="bi bi-arrow-bar-down"
+										class="bi bi-arrow-up"
 										viewBox="0 0 16 16"
 									>
 										<path
 											fill-rule="evenodd"
-											d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z"
+											d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
 										/>
 									</svg>
 								)}
