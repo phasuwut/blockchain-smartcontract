9d902558fa73e576c3893fd952e21470699233bb
phasuwut <44774302+phasuwut@users.noreply.github.com>
2022-04-30 15:47:03 +0700

    add deploy version 1

---
+++ Deploy/Version-1/scripts/interact.js
@@ -0,0 +1,32 @@
+// interact.js
+
+const API_KEY = process.env.API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+
+
+// For Hardhat 
+const contract = require("../artifacts/contracts/Lottery.sol/Lottery.json");
+console.log(JSON.stringify(contract.abi));
+
+// interact.js
+
+// Provider
+const alchemyProvider = new ethers.providers.AlchemyProvider(network="ropsten", API_KEY);
+
+// Signer
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+// Contract
+const lotteryContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
+
+// interact.js
+
+// ...
+
+async function main() {
+    const message = await lotteryContract.manager();
+    console.log("The message is: " + message);
+  }
+  main();
