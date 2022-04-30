8be5136fea153e5f6fb32d1562a77798d5fb8053
phasuwut <44774302+phasuwut@users.noreply.github.com>
2022-04-30 17:53:56 +0700

    update version 2 ดรืรห้

--- Deploy/Version-2/hardhat.config.js
+++ Deploy/Version-2/hardhat.config.js
@@ -1,20 +1,25 @@
-/**
-* @type import('hardhat/config').HardhatUserConfig
-*/
+// hardhat.config.js
 
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
 const { API_URL, PRIVATE_KEY } = process.env;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
-   solidity: "0.8.7",
-   defaultNetwork: "ropsten",
-   networks: {
+  solidity: "0.8.7",
+  defaultNetwork: "ropsten",
+  networks: {
       hardhat: {},
       ropsten: {
          url: API_URL,
          accounts: [`0x${PRIVATE_KEY}`]
       }
-   },
-}
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY
+  }
+};
