97c59de53f19312fa33c8cec0252805cb8edeae6
phasuwut <44774302+phasuwut@users.noreply.github.com>
2022-05-11 00:18:32 +0700

    update MyDetailpage

--- src/components/Pages/MyDetail/MyDetail.js
+++ src/components/Pages/MyDetail/MyDetail.js
@@ -1,9 +1,10 @@
 import { Accordion, Table } from "react-bootstrap";
 import React, { useEffect, useState } from "react";
-import { getMyDetailBuyer, getMyLotteryByPeriod } from "util/lottery";
+import { isRegistor as getIsRegistor, getMyDetailBuyer, getMyLotteryByPeriod } from "util/lottery";
 
 import Center from "components/Gobal/Center/Center";
 import Flex from "components/Gobal/Flex/Flex";
+import { Link } from "react-router-dom";
 import Loading from "components/Gobal/Loading/Loading";
 import MasterLayout from "components/Layout/MasterLayout/MasterLayout";
 import MyLottery from "components/Pages/MyDetail/components/MyLottery/MyLottery";
@@ -17,6 +18,7 @@ const MyDetail = () => {
 	const [myListLottery, setMyListLottery] = useState([]);
 	const [myPeriod, setMyPeriod] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [isRegistor, setIsRegistor] = useState(false);
 
 	useEffect(() => {
 		const fetchData = () => {
@@ -29,10 +31,6 @@ const MyDetail = () => {
 				if (MyAddress) {
 					setIsCannotMetamask(true);
 					setMyaddress(MyAddress);
-					getMyDetailBuyer(MyAddress).then((res) => {
-						//console.log(res);
-						setMyDetail(res);
-					});
 				} else {
 					setIsCannotMetamask(false);
 				}
@@ -45,6 +43,28 @@ const MyDetail = () => {
 			setBrowserIsCannotMetamask(false);
 		}
 	}, []);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const IsRegistor = await getIsRegistor(myAddress);
+			setIsRegistor(IsRegistor);
+		};
+
+		if (myAddress !== "") {
+			fetchData();
+		}
+	}, [myAddress]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const myDetailBuyer2 = await getMyDetailBuyer(myAddress);
+			setMyDetail(myDetailBuyer2);
+		};
+
+		if (isRegistor) {
+			fetchData();
+		}
+	}, [isRegistor]);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -85,143 +105,153 @@ const MyDetail = () => {
 				{browserIsCannotMetamask ? (
 					<>
 						{isCannotMetamask ? (
-							<div>
-								<hr />
-								<div className="flex justify-center">
-									<div className="w-11/12">
-										<div className="w-full">
-											<h1 style={{ textAlign: "center" }}> ข้อมูลของฉัน</h1>
+							<>
+								{!isRegistor ? (
+									<Center>
+										<Link to="/register">
+											<h1> Please register</h1>
+										</Link>
+									</Center>
+								) : (
+									<div>
+										<hr />
+										<div className="flex justify-center">
+											<div className="w-11/12">
+												<div className="w-full">
+													<h1 style={{ textAlign: "center" }}> ข้อมูลของฉัน</h1>
 
-											{isLoading ? (
-												<Loading />
-											) : (
-												<>
-													<Accordion defaultActiveKey="0">
-														<Accordion.Item eventKey="0">
-															<CustomAccordionHeader>
-																<Flex
-																	onClick={() => {
-																		console.log("0");
-																		if (showAccordion === "0") {
-																			setShowAccordion("");
-																		} else {
-																			setShowAccordion("0");
-																		}
-																	}}
-																>
-																	<h4>ข้อมูลต่างๆของฉัน</h4>
-																	{showAccordion === "0" ? (
-																		<svg
-																			xmlns="http://www.w3.org/2000/svg"
-																			width="50"
-																			height="50"
-																			fill="currentColor"
-																			class="bi bi-arrow-down"
-																			viewBox="0 0 16 16"
+													{isLoading ? (
+														<Loading />
+													) : (
+														<>
+															<Accordion defaultActiveKey="0">
+																<Accordion.Item eventKey="0">
+																	<CustomAccordionHeader>
+																		<Flex
+																			onClick={() => {
+																				console.log("0");
+																				if (showAccordion === "0") {
+																					setShowAccordion("");
+																				} else {
+																					setShowAccordion("0");
+																				}
+																			}}
 																		>
-																			<path
-																				fill-rule="evenodd"
-																				d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
-																			/>
-																		</svg>
-																	) : (
-																		<svg
-																			xmlns="http://www.w3.org/2000/svg"
-																			width="50"
-																			height="50"
-																			fill="currentColor"
-																			class="bi bi-arrow-up"
-																			viewBox="0 0 16 16"
-																		>
-																			<path
-																				fill-rule="evenodd"
-																				d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
-																			/>
-																		</svg>
-																	)}
-																</Flex>
-															</CustomAccordionHeader>
-															<Accordion.Body>
-																<CustomTable bordered hover responsive striped>
-																	<tbody>
-																		<tr>
-																			<td>myAddress</td>
-																			<td>{myAddress}</td>
-																		</tr>
+																			<h4>ข้อมูลต่างๆของฉัน</h4>
+																			{showAccordion === "0" ? (
+																				<svg
+																					xmlns="http://www.w3.org/2000/svg"
+																					width="50"
+																					height="50"
+																					fill="currentColor"
+																					class="bi bi-arrow-down"
+																					viewBox="0 0 16 16"
+																				>
+																					<path
+																						fill-rule="evenodd"
+																						d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+																					/>
+																				</svg>
+																			) : (
+																				<svg
+																					xmlns="http://www.w3.org/2000/svg"
+																					width="50"
+																					height="50"
+																					fill="currentColor"
+																					class="bi bi-arrow-up"
+																					viewBox="0 0 16 16"
+																				>
+																					<path
+																						fill-rule="evenodd"
+																						d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
+																					/>
+																				</svg>
+																			)}
+																		</Flex>
+																	</CustomAccordionHeader>
+																	<Accordion.Body>
+																		<CustomTable bordered hover responsive striped>
+																			<tbody>
+																				<tr>
+																					<td>myAddress</td>
+																					<td>{myAddress}</td>
+																				</tr>
 
-																		<tr>
-																			<td>First name</td>
-																			<td>{myDetail[0]}</td>
-																		</tr>
-																		<tr>
-																			<td>Last name </td>
-																			<td>{myDetail[1]}</td>
-																		</tr>
-																		<tr>
-																			<td>Email</td>
-																			<td>{myDetail[2]}</td>
-																		</tr>
-																	</tbody>
-																</CustomTable>
-															</Accordion.Body>
-														</Accordion.Item>
-														<hr />
+																				<tr>
+																					<td>First name</td>
+																					<td>{myDetail[0]}</td>
+																				</tr>
+																				<tr>
+																					<td>Last name </td>
+																					<td>{myDetail[1]}</td>
+																				</tr>
+																				<tr>
+																					<td>Email</td>
+																					<td>{myDetail[2]}</td>
+																				</tr>
+																			</tbody>
+																		</CustomTable>
+																	</Accordion.Body>
+																</Accordion.Item>
+																<hr />
 
-														<Accordion.Item eventKey="1">
-															<CustomAccordionHeader>
-																<Flex
-																	onClick={() => {
-																		console.log("1");
-																		if (showAccordion === "1") {
-																			setShowAccordion("");
-																		} else {
-																			setShowAccordion("1");
-																		}
-																	}}
-																>
-																	<h4>ข้อมูลการซื้อหวยของฉัน</h4>
-																	{showAccordion === "1" ? (
-																		<svg
-																			xmlns="http://www.w3.org/2000/svg"
-																			width="50"
-																			height="50"
-																			fill="currentColor"
-																			class="bi bi-arrow-down"
-																			viewBox="0 0 16 16"
+																<Accordion.Item eventKey="1">
+																	<CustomAccordionHeader>
+																		<Flex
+																			onClick={() => {
+																				console.log("1");
+																				if (showAccordion === "1") {
+																					setShowAccordion("");
+																				} else {
+																					setShowAccordion("1");
+																				}
+																			}}
 																		>
-																			<path
-																				fill-rule="evenodd"
-																				d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
-																			/>
-																		</svg>
-																	) : (
-																		<svg
-																			xmlns="http://www.w3.org/2000/svg"
-																			width="50"
-																			height="50"
-																			fill="currentColor"
-																			class="bi bi-arrow-up"
-																			viewBox="0 0 16 16"
-																		>
-																			<path
-																				fill-rule="evenodd"
-																				d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
-																			/>
-																		</svg>
-																	)}
-																</Flex>
-															</CustomAccordionHeader>
-															<Accordion.Body>
-																<MyLottery myListLottery={myListLottery} myPeriod={myPeriod} />
-															</Accordion.Body>
-														</Accordion.Item>
-													</Accordion>
-												</>
-											)}
+																			<h4>ข้อมูลการซื้อหวยของฉัน</h4>
+																			{showAccordion === "1" ? (
+																				<svg
+																					xmlns="http://www.w3.org/2000/svg"
+																					width="50"
+																					height="50"
+																					fill="currentColor"
+																					class="bi bi-arrow-down"
+																					viewBox="0 0 16 16"
+																				>
+																					<path
+																						fill-rule="evenodd"
+																						d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+																					/>
+																				</svg>
+																			) : (
+																				<svg
+																					xmlns="http://www.w3.org/2000/svg"
+																					width="50"
+																					height="50"
+																					fill="currentColor"
+																					class="bi bi-arrow-up"
+																					viewBox="0 0 16 16"
+																				>
+																					<path
+																						fill-rule="evenodd"
+																						d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
+																					/>
+																				</svg>
+																			)}
+																		</Flex>
+																	</CustomAccordionHeader>
+																	<Accordion.Body>
+																		<MyLottery myListLottery={myListLottery} myPeriod={myPeriod} />
+																	</Accordion.Body>
+																</Accordion.Item>
+															</Accordion>
+														</>
+													)}
+												</div>
+											</div>
 										</div>
 									</div>
-								</div>
-							</div>
+								)}
+							</>
 						) : (
 							<Center>
 								<h1> Please Connect metamask</h1>
